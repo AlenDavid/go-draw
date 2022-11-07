28c0ef09bcbb32115b534aa1f42fc903b2b8bc7b
David Alen <davidalen.dev@gmail.com>
2022-11-07 00:07:38 -0300

    refactor: remove func in prol of package func

--- packages/actions/translation.go
+++ packages/actions/translation.go
@@ -5,22 +5,14 @@ import (
 	"image/color"
 )
 
-func isOutOfBounds(img image.Image, x, y int) bool {
-	if x < 0 || y < 0 {
-		return true
-	}
-
-	return x >= img.Bounds().Size().X || y >= img.Bounds().Size().Y
-}
-
 func Translation(img image.Image, translationX, translationY int) image.Image {
 	m := image.NewRGBA(image.Rect(0, 0, img.Bounds().Size().X, img.Bounds().Size().Y))
 
 	for i := 0; i < img.Bounds().Size().X; i += 1 {
 		for j := 0; j < img.Bounds().Size().Y; j += 1 {
-			// if translation is out of bounds, set white and continue
-			if isOutOfBounds(img, i-translationX, j-translationY) {
-				m.Set(i, j, color.White)
+			// if translation is out of bounds, set black and continue
+			if !image.Pt(i-translationX, j-translationY).In(img.Bounds()) {
+				m.Set(i, j, color.Black)
 				continue
 			}
 
