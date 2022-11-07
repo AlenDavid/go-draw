f8212c9482fbc0e499ddeaf8f6cd5ff0748d9f27
David Alen <davidalen.dev@gmail.com>
2022-11-06 21:21:33 -0300

    add: savefile option

--- packages/ui/buttons/openFile.go
+++ packages/ui/buttons/openFile.go
@@ -3,6 +3,7 @@ package buttons
 import (
 	"fmt"
 	"image"
+	"image/png"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/canvas"
@@ -42,32 +43,28 @@ func OpenFile(app fyne.App, container *fyne.Container, storage storage.Storage, 
 	})
 }
 
-// TODO: work on this function
 func SaveFile(app fyne.App, container *fyne.Container, storage storage.Storage, rebuild func()) *widget.Button {
 	return widget.NewButtonWithIcon("Save file", theme.FileImageIcon(), func() {
 		window := app.Driver().AllWindows()[0]
 
 		window.Show()
 
-		dialog.NewFileOpen(func(uc fyne.URIReadCloser, err error) {
-			fmt.Println("Dialog done with interactions.")
+		dialog.NewFileSave(func(uc fyne.URIWriteCloser, err error) {
 
 			if err != nil || uc == nil {
 				fmt.Println(err, uc)
 				return
 			}
 
-			container.Remove(container.Objects[0])
+			defer uc.Close()
 
-			img2, _, _ := image.Decode(uc)
-			storage.SetImage(img2)
+			if err, img := storage.GetWorkingImage(); err == nil && img != nil {
+				if err := png.Encode(uc, img); err != nil {
+					fmt.Println("error encoding image")
+				}
+			}
 
-			newImageCanvas := canvas.NewImageFromImage(img2)
-			newImageCanvas.Resize(fyne.NewSize(float32(img2.Bounds().Size().X), float32(img2.Bounds().Size().Y)))
-
-			container.Refresh()
-			rebuild()
-			container.Add(newImageCanvas)
+			fmt.Println("Dialog done with interactions.")
 
 		}, window).Show()
 	})
