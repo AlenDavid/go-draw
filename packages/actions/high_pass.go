b7360b7f624a3fb3695e2c98f15497cfc3622236
David Alen <davidalen.dev@gmail.com>
2022-12-01 11:18:48 -0300

    add: roberts

--- packages/actions/high_pass.go
+++ packages/actions/high_pass.go
@@ -3,16 +3,40 @@ package actions
 import (
 	"image"
 	"image/color"
+	"math"
+)
+
+const (
+	HighPassRobertsThreshold = 100
 )
 
 func HighPassRoberts(img image.Image) image.Image {
 	bounds := img.Bounds()
 	m := image.NewRGBA(bounds)
 
+	xKernel := [][]int{{0, 0, 0}, {0, -1, 0}, {0, 0, 1}}
+	yKernel := [][]int{{0, 0, 0}, {0, 0, -1}, {0, 1, 0}}
+
 	for i := 0; i < bounds.Dx(); i += 1 {
 		for j := 0; j < bounds.Dy(); j += 1 {
+			var r uint8
+			var gx, gy int = 0, 0
 
-			color := color.RGBA{255, 255, 255, 255}
+			for k := 0; k < 3; k += 1 {
+				for l := 0; l < 3; l += 1 {
+					color := img.At(i+(k-1), j+(l-1))
+					R, _, _, _ := color.RGBA()
+
+					gx += int(R/257) * xKernel[k][l]
+					gy += int(R/257) * yKernel[k][l]
+				}
+			}
+
+			if math.Sqrt(math.Pow(float64(gx), 2)+math.Pow(float64(gy), 2)) > HighPassRobertsThreshold {
+				r = 255
+			}
+
+			color := color.RGBA{r, r, r, 255}
 			m.Set(i, j, color)
 		}
 	}
